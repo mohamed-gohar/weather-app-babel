ccde684d3e1cb71e98b6b95c951a3e7a7b351a58
mohamed-gohar <mohamedgohar37@gmail.com>
2021-06-13 22:43:22 +0200

    'vercel.json'

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ const server = app.listen(port, () => {
 });
 
 //set routes
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/website/index.html"));
+});
+
 app.get("/getData", function (req, res) {
   res.send(projectData);
 });
